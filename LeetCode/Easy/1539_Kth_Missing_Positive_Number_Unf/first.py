6cad3315f0871ea104d850176a34f09f59545d25
Matt Hatter <matt_hatter@DESKTOP-68UL55P.localdomain>
2021-01-06 20:12:11 -0600

    unfinished problems

---
+++ LeetCode/Easy/1539_Kth_Missing_Positive_Number_Unf/first.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findKthPositive(self, arr, k):
+            
+
+def main():
+    print('test')
+
+if __name__ == '__main__':
+    main()
---
+++ LeetCode/Medium/82_Remove_Duplicates_Sorted_LL_2_Unf/first.py
@@ -0,0 +1,14 @@
+class ListNode(object):
+    def __init_(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        print('test')
+
+def main():
+    print('test')
+
+if __name__ == '__main__':
+    main()
